3fe93159b3b520c63f8b24a2cc8151c912f08caa
Droxus <alexredov@gmail.com>
2022-02-23 21:59:34 +0200

    +bg +new styles

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-// import './style.css';
+
 //  --- --- --- --- --- ---  Инициализация --- --- --- --- --- --- --- ---
 
 const body = document.body
+const root = document.querySelector(':root')
 const btnSignIn = document.getElementById('btnSignIn')
 const btnSignUp = document.getElementById('btnSignUp')
 const signIn = Array.from(document.getElementsByClassName('signIn'))
@@ -15,17 +16,18 @@ const inptPassword = document.getElementById('inptPassword')
 const mainBlock = document.getElementById('mainBlock')
 const nickForLabel = document.getElementById('nickForLabel')
 const btnHome = document.getElementById('btnHome')
-const btnMyTournaments = document.getElementById('btnMyTournaments')
-const btnDataEnter = document.getElementById('btnDataEnter')
+const btnFavorites = document.getElementById('btnFavorites')
+const btnData = document.getElementById('btnData')
 const btnStatistics = document.getElementById('btnStatistics')
 const btnAbout = document.getElementById('btnAbout')
 const contentBlock = document.getElementById('contentBlock')
 const labelUnity = document.getElementById('labelUnity')
 const navigationPanel = document.getElementById('navigationPanel')
 const profile = document.getElementById('profile')
-
-
-const profileWidth = 200
+const headerBlock = document.getElementById('headerBlock')
+const profIcon = document.getElementById('profIcon')
+const NavPanelBtns = [btnHome, btnFavorites, btnData, btnStatistics, btnAbout, profile]
+let profileWidth = 200
 const labelUnityWidth = 450
 let password, email, width, height
 noaccLink.addEventListener('click', onNoaccLink)
@@ -33,10 +35,11 @@ hasaccLink.addEventListener('click', onHasaccLink)
 btnSignIn.addEventListener('click', onBtnSignIn)
 btnSignUp.addEventListener('click', onBtnSignUp)
 btnHome.addEventListener('click', onbtnHome)
-btnMyTournaments.addEventListener('click', onbtnMyTournaments)
-btnDataEnter.addEventListener('click', onbtnDataEnter)
+btnFavorites.addEventListener('click', onbtnFavorites)
+btnData.addEventListener('click', onbtnData)
 btnStatistics.addEventListener('click', onbtnStatistics)
 btnAbout.addEventListener('click', onbtnAbout)
+profile.addEventListener('click', onProfile)
 width = window.innerWidth
 height = window.innerHeight
 function onresize() {
@@ -81,7 +84,7 @@ function showMainPage(){
 }
 function getNickName() {
   let login = email
-  login = email.slice(0, email.indexOf('@')).toUpperCase() 
+  login = email[0].toUpperCase() + email.slice(1, email.indexOf('@')).toLowerCase()
   nickForLabel.innerHTML = login
 }
 function onSignIn(){
@@ -98,49 +101,83 @@ function onSignIn(){
 
 function onbtnHome(event){
   contentBlock.innerHTML = "<h1> You at Home page now </h1>"
+  clearMenu()
+  btnHome.style["border-bottom"] = '2px solid black'
+  btnHome.style.color = 'black'
 }
-function onbtnMyTournaments(event){
-  contentBlock.innerHTML = "<h1> You at My Tournaments page now </h1>"
+function onbtnFavorites(event){
+  contentBlock.innerHTML = "<h1> You at Favorites page now </h1>"
+  clearMenu()
+  btnFavorites.style["border-bottom"] = '2px solid black'
+  btnFavorites.style.color = 'black'
 }
-function onbtnDataEnter(event){
-  contentBlock.innerHTML = "<h1> You at Data Enter page now </h1>"
+function onbtnData(event){
+  contentBlock.innerHTML = "<h1> You at Data page now </h1>"
+  clearMenu()
+  btnData.style["border-bottom"] = '2px solid black'
+  btnData.style.color = 'black'
 }
 function onbtnStatistics(event){
   contentBlock.innerHTML = "<h1> You at Statistics page now </h1>"
+  clearMenu()
+  btnStatistics.style["border-bottom"] = '2px solid black'
+  btnStatistics.style.color = 'black'
 }
 function onbtnAbout(event){
   contentBlock.innerHTML = "<h1> You at About page now </h1>"
+  clearMenu()
+  btnAbout.style["border-bottom"] = '2px solid black'
+  btnAbout.style.color = 'black'
 }
-
+function clearMenu(){
+  NavPanelBtns.forEach(element => element.style["border-bottom"] = '0px solid black')
+  NavPanelBtns.forEach(element => element.style.color = 'white')
+}
 checkAspectRatio()
 function checkAspectRatio(){
   if (height > width) {
+    root.style.setProperty('--headerHeight', '100px')
+    root.style.setProperty('--profile-width', '150px')
+    NavPanelBtns.forEach(element => element.style["margin-top"] = 'calc(var(--headerHeight) - (var(--height-NavPanel) + 44px))')
+    NavPanelBtns.forEach(element => element.style.font = '24px "Fira Sans", sans-serif')
+    NavPanelBtns.forEach(element => element.style["text-shadow"] = '1px 0 black, 0 1px black, -1px 0 black, 0 -1px black')
     labelUnity.innerHTML = ' '
-    navigationPanel.style["margin-top"] = 0
-    navigationPanel.style["margin-left"] = 0
-    labelUnity.style["border-right"] = '0px dashed black'
-    navigationPanel.style.width = width - profileWidth
-    profile.style["margin-top"] = 0
-    profile.style["margin-left"] = width - profileWidth + 3
+    profile.style["margin-left"] = 'calc(100% - var(--profile-width))'
+    profile.style["margin-top"] = 'calc(var(--headerHeight) - var(--height-Profile))'
+    profIcon.style["margin-top"] = '-10px'
+    profile["font-size"] = '18px' //?
+    profIcon.style.width = '36px'
+    navigationPanel.style["margin-left"] = '0px'
+    navigationPanel.style.width = 'calc(100% - var(--profile-width))'
 
   } else {
+    root.style.setProperty('--headerHeight', '200px')
+    root.style.setProperty('--profile-width', '200px')
+    NavPanelBtns.forEach(element => element.style["margin-top"] = 'calc(var(--headerHeight) - (var(--height-NavPanel) + 54px))')
+    NavPanelBtns.forEach(element => element.style.font = '32px "Fira Sans", sans-serif')
+    NavPanelBtns.forEach(element => element.style["text-shadow"] = '1px 0 black, 0 1px black, -1px 0 black, 0 -1px black')
+    contentBlock.style["margin-top"] = 'calc(var(--headerHeight) + 50px)'  
+    navigationPanel.style["margin-left"] = 'calc(var(--labelUnity-width))'
+    navigationPanel.style.width = 'calc(100% - var(--profile-width) - var(--labelUnity-width))'
+    profile.style["margin-left"] = 'calc(100% - var(--profile-width))'
+    profile["font-size"] = '24px'
+    profile.style["margin-top"] = 'calc(var(--headerHeight) - var(--height-Profile))'
+    profIcon.style["margin-top"] = '10px'
+    profIcon.style.width = '48px'
     labelUnity.innerHTML = ` <div class="labelUnity" id="labelUnity">
     <label class="tileLbl" id="tileLbl1" for="tileLbl"><b>Online</b></label>
     <label class="tileLbl" id="tileLbl2" for="tileLbl"><b>Competition</b></label>
     <label class="tileLbl" id="tileLbl3" for="tileLbl"><b>Table</b></label>
     <label class="tileLbl" id="tileLbl4" for="tileLbl">by Droxus corporation</label>
 </div>`
-    labelUnity.style["border-right"] = '2px dashed black'
-    navigationPanel.style["margin-top"] = 0
-    navigationPanel.style["margin-left"] = labelUnityWidth + 3
-    navigationPanel.style.width = width - profileWidth - labelUnityWidth + 5
-    profile.style["margin-top"] = 0
-    profile.style["margin-left"] = width - profileWidth + 10
   }
-
 }
-
-
+function onProfile() {
+  contentBlock.innerHTML = "<h1> You at Profile page now </h1>"
+  clearMenu()
+  profile.style["border-bottom"] = '2px solid black'
+  profile.style.color = 'black'
+}
 //--- --- --- --- --- ---  firebase --- --- --- --- --- --- --- ---
 
 import firebase from 'firebase/compat/app';
